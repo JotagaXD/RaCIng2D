533f35266d16366515f01b258ee67cfe4f78fcb7
João Henrique Moraes Guedes <158523563+JotagaXD@users.noreply.github.com>
2024-08-15 22:14:18 -0300

    Update interactions.py

--- interactions.py
+++ interactions.py
@@ -1,16 +1,16 @@
 import pygame
 
-def no_control(player, oil_sprites):
+def no_control(player, oil_sprites):  # função que sinaliza a colisão do carrinho com o sprite do oleo
     if pygame.sprite.spritecollide(player, oil_sprites, False, pygame.sprite.collide_mask):
         return True
     else:
         return False
 
-def slow(player, slow_sprites):
+def slow(player, slow_sprites):   # função que sinaliza a colisão do carrinho com o sprite do slow
     if pygame.sprite.spritecollide(player, slow_sprites, False, pygame.sprite.collide_mask):
         return True
     else:
         return False
 
-def difficult(lapse):
+def difficult(lapse):  # função que aumentará a dificuldade do jogo em função do tempo
     return int(lapse*(9/10))
