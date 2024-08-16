429667732ee706f7df589f3d4b5524d1ae2a09a1
João Henrique Moraes Guedes <158523563+JotagaXD@users.noreply.github.com>
2024-08-15 22:15:58 -0300

    Update score_menu.py

--- score_menu.py
+++ score_menu.py
@@ -1,6 +1,6 @@
 import pygame
 import screen
-def scorepage(HIGHSCORE_label_nomes,HIGHSCORE_label_pontos,WINDOW_WIDTH, WINDOW_HEIGHT,fontebase):
+def scorepage(HIGHSCORE_label_nomes,HIGHSCORE_label_pontos,WINDOW_WIDTH, WINDOW_HEIGHT,fontebase):  # função responsável pelo funcionamento da tela de scores
     display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     display_surface.fill((0, 0, 0,))
     print_y=180
@@ -18,12 +18,13 @@ def scorepage(HIGHSCORE_label_nomes,HIGHSCORE_label_pontos,WINDOW_WIDTH, WINDOW_
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        if pygame.key.get_just_pressed()[pygame.K_m]:
+        if pygame.key.get_just_pressed()[pygame.K_m]:   # chamada da função responsável pelo funcionamento da tela de início, a qual retornará um bool, que sinaliza a interação do usário, podendo ser de saída ou início do jogo
             if screen.init(display_surface,clock,WINDOW_WIDTH,WINDOW_HEIGHT,HIGHSCORE_label_pontos,HIGHSCORE_label_nomes,fontebase):
                 return True
             else:
                 return False
         if contador == 0:
+            # for loops que varrem o arquivo, obtendo os valores existentes dentro dele
             for i in HIGHSCORE_label_nomes:
                 nomes = fontebase.render(f"{i}", False, (255, 125, 37))
                 display_surface.blit(nomes, (450, print_y))
